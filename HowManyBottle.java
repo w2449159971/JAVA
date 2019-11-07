3f98b66f1830a850fdd2a10172d9462f5be4fb14
w2449159971 <2449159971@qq.com>
2019-11-07 21:58:09 +0800

    每三个空瓶换一瓶饮料 最多喝多少瓶饮料

---
+++ HowManyBottle.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class HowManyBottle {
+    public static int BottleNumber(int k){
+        int count = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while(k >= 3){
+            a = k / 3;
+            b = k % 3;
+            c = count;
+            count = a + count;
+            k = b + count - c;
+        }
+        if(k==2){
+            count ++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        while(k.hasNext()){
+            int num = k.nextInt();
+            if(num !=0){
+                System.out.println(BottleNumber(num));
+    }
+        }
+    }
+}
