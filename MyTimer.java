00b0a02167893619fd24d069d9895876dc2c6c9d
w2449159971 <2449159971@qq.com>
2019-11-28 21:31:27 +0800

    定时器

---
+++ MyTimer.java
@@ -0,0 +1,30 @@
+package lesson2;
+
+public class MyTimer {
+    public static void main(String[] args) {
+        new MyTimer().schedule(new Runnable(){
+            @Override
+            public void run(){
+                System.out.println("嘻嘻");
+            }
+    },0,1000);
+}
+public void schedule(Runnable task, long delay, long period){
+    try{
+        long next = System.currentTimeMillis() + delay;
+        while (true){
+            long current = System.currentTimeMillis();
+            if(next > current) {
+                Thread.sleep(next - current);
+            }
+            new Thread(task).start();
+                if(period > 0)
+                    next += period;
+                else
+                    break;
+            }
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+}
+}
